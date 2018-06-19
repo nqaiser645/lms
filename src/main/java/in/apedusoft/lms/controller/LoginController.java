5a6ba01ddeaea25cf673a942d58cc526390a4c2c
nqaiser645 <nqaiser645@gmail.com>
2018-06-19 09:57:16 +0530

    chan

--- src/main/java/in/apedusoft/lms/controller/LoginController.java
+++ src/main/java/in/apedusoft/lms/controller/LoginController.java
@@ -1,15 +1,11 @@
 package in.apedusoft.lms.controller;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import in.apedusoft.lms.model.UserLogin;
 
@@ -19,33 +15,8 @@ public class LoginController {
 
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(HttpServletRequest req, HttpServletResponse res) {
-		ModelAndView mav = new ModelAndView("login");
-		mav.addObject("login", new UserLogin());
-		return "mav";
+	public String login() {
+		
+		return "login";
 	}
-
-	@RequestMapping(value="/loginProcess", method=RequestMethod.GET)
-	public ModelAndView loginProcess(HttpServletRequest req, HttpServletResponse res) {
-
-		Public String loginProcess(Model model, @ModelAttribute("UserLogin") UserLogin userLogin) {
-			if (userLogin != null && userLogin.getUserName() != null & userLogin.getPassword() != null) {
-				if (userLogin.getUserName().equals("chandra") && userLogin.getPassword().equals("chandra123")) {
-					model.addAttribute("msg", userLogin.getUserName());
-					return "index";
-				} else {
-					model.addAttribute("error", "Invalid Details");
-					return "login";
-				}
-			} else {
-				model.addAttribute("error", "Please enter Details");
-				return "login";
-			}
-		}
-
-
-	}
-
-
-
 }
